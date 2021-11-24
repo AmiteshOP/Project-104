3ac6bddf74417906d2053af7ddc56f520fb1a05d
AmiteshOP <88427048+AmiteshOP@users.noreply.github.com>
2021-11-24 13:02:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format : 'png',
+    png_quality: 90
+});
+
+Webcam.attach( '#camera' );
+camera = document.getElementById("camera");
+
+
+function take_snapsho()
+{
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/train/image',modelLoaded);
